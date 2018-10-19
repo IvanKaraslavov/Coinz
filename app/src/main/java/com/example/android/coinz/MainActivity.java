7797200e751fdbf9eb236903f8555571a255d012
s1632798 <s1632798@sms.ed.ac.uk>
2018-10-19 17:10:00 +0100

    Added navigation drawable template to the map

--- app/src/main/java/com/example/android/coinz/MainActivity.java
+++ app/src/main/java/com/example/android/coinz/MainActivity.java
@@ -4,14 +4,24 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineListener;
@@ -46,7 +56,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         OnMapReadyCallback, LocationEngineListener,
-        PermissionsListener {
+        PermissionsListener,NavigationView.OnNavigationItemSelectedListener {
 
     private String tag = "MainActivity";
     private String downloadDate = ""; // Format: YYYY/MM/DD
@@ -118,6 +128,22 @@ public class MainActivity extends AppCompatActivity implements
         mapView = findViewById(R.id.mapboxMapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Button mapButton = findViewById(R.id.button);
+        mapButton.setOnClickListener(view -> drawer.openDrawer(Gravity.START));
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Oswald-Bold.ttf");
+        TextView customFontTextView = findViewById(R.id.hello);
+        customFontTextView.setTypeface(typeface);
     }
 
     @SuppressLint("LogNotTimber")
@@ -173,7 +199,7 @@ public class MainActivity extends AppCompatActivity implements
         }
         else {
             //Load map from the downloaded file
-            String geoJsonString = "";
+            String geoJsonString;
             setFileDownloaded(true);
             try {
                 FileInputStream fis = openFileInput("coinzmap.geojson");
@@ -360,5 +386,61 @@ public class MainActivity extends AppCompatActivity implements
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
